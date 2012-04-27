2516a3c1a3e1624656924a745f4c002cf903a4f5
Hyogi Sim <hyogi@vt.edu>
2012-04-27 16:50:05 -0400

    Cache framework implemented.

--- src/cache.c
+++ src/cache.c
@@ -1,6 +1,6 @@
 /* Copyright (C) Hyogi Sim <hyogi@cs.vt.edu>
  * 
- * --------------------------------------------------------------------------
+ * ---------------------------------------------------------------------------
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
@@ -16,43 +16,41 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  * 
  */
-#ifndef	__CACHE_H__
-#define	__CACHE_H__
+#include <stdlib.h>
+#include <string.h>
 
-#include <linux/types.h>
-
-#include "device.h"
-
-struct cache;
-
-struct cache_stat {
-	__u64	hit_count;
-	__u64	miss_count;
-};
-
-struct cache_ops {
-	int (*init) (struct cache *self);
-	void (*exit) (struct cache *self);
-	int (*get_stat) (struct cache *self, struct cache_stat *stat);
-	int (*read_block) (struct cache *self, __u32 req_node,
-				__u64 offset, __u64 count, double *latency);
-	int (*write_block) (struct cache *self, __u32 req_node,
-				__u64 offset, __u64 count, double *latency);
-};
-
-struct cache {
-	int ndevs;
-	struct stdev **devs;
-	struct cache_stat stat;
-	struct cache_ops *ops;
-
-	void *private;
-};
+#include "cache.h"
 
 struct cache *cache_init(struct stdev **devs, int ndevs,
-			struct cache_ops *ops, void *private);
+			struct cache_ops *ops, void *private)
+{
+	struct cache *self;
 
-void cache_exit(struct cache *self);
+	if (devs == NULL || ndevs < 0 || ops == NULL)
+		return NULL;
 
-#endif	/* __CACHE_H__ */
+	self = malloc(sizeof(*self));
+	if (self) {
+		self->ndevs = ndevs;
+		self->devs = devs;
+		memset(&self->stat, 0, sizeof(self->stat));
+		self->private = private;
+
+		if (self->ops->init)
+			if (self->ops->init(self) < 0) {
+				free(self);
+				self = NULL;
+			}
+	}
+	return self;
+}
+
+void cache_exit(struct cache *self)
+{
+	if (self) {
+		if (self->ops->exit)
+			self->ops->exit(self);
+		free(self);
+	}
+}
 
