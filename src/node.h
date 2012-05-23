baedb159c7e492eb175eca3383e3966a0e34c2e1
Hyogi Sim <sandrain@gmail.com>
2012-05-23 13:31:32 -0400

    documentation for headers are done.

--- src/node.h
+++ src/node.h
@@ -1,3 +1,21 @@
+/* Copyright (C) Hyogi Sim <hyogi@cs.vt.edu>
+ * 
+ * ---------------------------------------------------------------------------
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ */
 #ifndef	__NODE_H__
 #define	__NODE_H__
 
@@ -5,51 +23,113 @@
 
 #define	PFS_NODE_ID	0
 
+/**
+ * Not used currently..
+ */
 struct node_operations {
 };
 
+/**
+ * Abstraction of a node.
+ */
 struct node {
-	__u32 id;
+	__u32 id;			/* id of this node */
 
-	struct ioapp *app;
-	struct local_cache *cache;
+	struct ioapp *app;		/* application which runs here */
+	struct local_cache *cache;	/* local cache */
 
-	struct storage *ram;
+	struct storage *ram;		/* storage devices in this node */
 	struct storage *ssd;
 	struct storage *hdd;
 
-	struct node *pfs;
+	struct node *pfs;		/* connection to the pfs node */
 
-	void *private;
+	void *private;			/* free hook */
 
-	struct node_operations *ops;
+	struct node_operations *ops;	/* operations, currently not used */
 };
 
+/** these are not used, yet */
 extern struct node_operations compute_node_operations;
 extern struct node_operations pfs_node_operations;
 
+/**
+ * node_init initializes the node instance. NOTE that @cache and @pfs should be
+ * set using other functions. It is prefered to use node_init_compute() or
+ * node_init_pfs() below instead of directly using this function.
+ *
+ * @self: memory space which should be allocated by the caller.
+ * @id: node id assigned to this node.
+ * @app: application instance.
+ * @ram: ram device.
+ * @ssd: ssd device.
+ * @hdd: hdd device.
+ * @ops: operation table.
+ * @private: free pointer hook, which can be used by the caller.
+ *
+ * returns initialized node instance. NULL on error.
+ *
+ * FIXME: this function requires too many arguments.
+ */
 struct node *node_init(struct node *self,
 		__u32 id, struct ioapp *app,
 		struct storage *ram, struct storage *ssd, struct storage *hdd,
 		struct node_operations *ops, void *private);
 
+/**
+ * node_exit de-initializes the node instance.
+ *
+ * @self: node instance.
+ */
 static inline void node_exit(struct node *self) {}
 
+/**
+ * node_set_local_cache sets the local_cache instance of the given node.
+ *
+ * @self: node instance.
+ * @cache: local_cache instance.
+ */
 static inline
 void node_set_local_cache(struct node *self, struct local_cache *cache)
 {
 	self->cache = cache;
 }
 
+/**
+ * node_init_compute is a wrapper of node_init for initializing a computing
+ * node. @app, @ram, @ssd, and @hdd should be properly initialized before
+ * calling this function.
+ *
+ * @self: should be allocated by the caller.
+ * @id: node id to be set.
+ * @app: application instance.
+ * @ram: ram device.
+ * @ssd: ssd device.
+ * @hdd: hdd device.
+ *
+ * returns node instance on success, NULL otherwise.
+ */
 static inline
-struct node *node_init_compute(struct node *self,
-		__u32 id, struct ioapp *app,
+struct node *node_init_compute(struct node *self, __u32 id, struct ioapp *app,
 		struct storage *ram, struct storage *ssd, struct storage *hdd)
 {
 	return node_init(self, id, app, ram, ssd, hdd,
 			&compute_node_operations, NULL);
 }
 
+/**
+ * node_init_pfs is a wrapper for a pfs node. All device arguments should be
+ * initialized before calling this. There is no application instance in pfs
+ * node. In simulation, only one pfs node should be used and the id of pfs node
+ * is always 0.
+ *
+ * @self: should be allocated by the caller.
+ * @ram: ram device.
+ * @ssd: ssd device.
+ * @hdd: hdd device.
+ *
+ * return node instance on success, NULL otherwise.
+ */
 static inline
 struct node *node_init_pfs(struct node *self,
 		struct storage *ram, struct storage *ssd, struct storage *hdd)
@@ -58,6 +138,12 @@ struct node *node_init_pfs(struct node *self,
 			&pfs_node_operations, NULL);
 }
 
+/**
+ * node_set_pfs sets the pfs instance for a node.
+ *
+ * @self: node instance.
+ * @pfs: pfs instance.
+ */
 static inline
 void node_set_pfs(struct node *self, struct node *pfs)
 {
@@ -65,10 +151,33 @@ void node_set_pfs(struct node *self, struct node *pfs)
 		self->pfs = pfs;
 }
 
+/**
+ * node_service_ioapp fetches and services block io requests from the
+ * @self->app.
+ *
+ * @self: node instance
+ *
+ * returns 0.
+ */
 int node_service_ioapp(struct node *self);
 
+/**
+ * node_pfs_rw_block would be used when you need to request block io operation
+ * to the pfs node.
+ *
+ * @pfs: pfs node instance.
+ * @req: block io request.
+ *
+ * returns number of hits in pfs cache, <0 on error.
+ */
 int node_pfs_rw_block(struct node *pfs, struct io_request *req);
 
+/**
+ * node_get_statistics fills @stat structure with current node statistics.
+ *
+ * @self: node instance.
+ * @stat: [out] stat structure to be filled.
+ */
 void node_get_statistics(struct node *self, struct node_statistics *stat);
 
 #endif	/** __NODE_H__ */
--- src/storage.h
+++ src/storage.h
@@ -1,3 +1,21 @@
+/* Copyright (C) Hyogi Sim <hyogi@cs.vt.edu>
+ * 
+ * ---------------------------------------------------------------------------
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ */
 #ifndef	__STORAGE_H__
 #define	__STORAGE_H__
 
@@ -6,33 +24,75 @@
 struct storage;
 struct io_request;
 
+/**
+ * storage_operations contains methods of storage instance. If your device
+ * needs its own io methods you will define this.
+ * @read_block and @write_block takes the following argument.
+ *
+ * @self: storage instance.
+ * @offset: starting block number.
+ * @len: number of blocks.
+ *
+ * They should return 0 on success and <0 on errors.
+ */
 struct storage_operations {
 	int (*read_block) (struct storage *self, __u64 offset, __u64 len);
 	int (*write_block) (struct storage *self, __u64 offset, __u64 len);
 };
 
+/**
+ * Use this for the general block devices.
+ */
 extern struct storage_operations generic_storage_ops;
 
+/**
+ * storage abstracts a general block device and ram as well. The block size is
+ * globally define and can be fetched by cachesim_config->block_size.
+ */
 struct storage {
-	__u32 node;
+	__u32 node;			/* node id to which this belongs */
 
-	__u64 block_count;
+	__u64 block_count;		/* number of blocks: capacity */
 
-	__u32 latency_read;
-	__u32 latency_write;
+	__u32 latency_read;		/* read latency */
+	__u32 latency_write;		/* write latency */
 
-	__u64 stat_reads;
+	__u64 stat_reads;		/* statistics */
 	__u64 stat_writes;
 
-	struct storage_operations *ops;
+	struct storage_operations *ops; /* Operation table */
 
 	__u64 *wear;
 };
 
+/**
+ * storage_init initializes a new storage instance. It is prefered to use
+ * device specific initialization methods listed below.
+ *
+ * @self: should be allocated by the caller.
+ * @node: node id of this device.
+ * @block_count: number of available blocks.
+ * @latency_read: read latency.
+ * @latency_write: write latency.
+ * @wear: do we need to keep track of wear information for this device?
+ * @ops: operation table.
+ *
+ * returns a new storage instance, NULL on errors.
+ */
 struct storage *storage_init(struct storage *self, __u32 node, __u64
 			block_count, __u32 latency_read, __u32 latency_write,
 			int wear, struct storage_operations *ops);
 
+/**
+ * storage_init_ram is a wrapper for ram device. Since we simulate cache
+ * systems ram is also regarded as a block device.
+ *
+ * @self: should be allocated by the caller.
+ * @node: node id.
+ * @block_count: number of blocks available.
+ *
+ * returns a new storage instance, NULL on errors.
+ */
 static inline struct storage *storage_init_ram(struct storage *self, __u32 node,
 				__u64 block_count)
 {
@@ -45,6 +105,15 @@ static inline struct storage *storage_init_ram(struct storage *self, __u32 node,
 				&generic_storage_ops);
 }
 
+/**
+ * storage_init_ssd, a wrapper for ssd device.
+ *
+ * @self: should be allocated by the caller.
+ * @node: node id.
+ * @block_count: number of blocks available.
+ *
+ * returns a new storage instance, NULL on errors.
+ */
 static inline struct storage *storage_init_ssd(struct storage *self, __u32 node,
 				__u64 block_count)
 {
@@ -57,6 +126,15 @@ static inline struct storage *storage_init_ssd(struct storage *self, __u32 node,
 				&generic_storage_ops);
 }
 
+/**
+ * storage_init_ssd, a wrapper for ssd device.
+ *
+ * @self: should be allocated by the caller.
+ * @node: node id.
+ * @block_count: number of blocks available.
+ *
+ * returns a new storage instance, NULL on errors.
+ */
 static inline struct storage *storage_init_hdd(struct storage *self, __u32 node,
 				__u64 block_count)
 {
@@ -69,9 +147,24 @@ static inline struct storage *storage_init_hdd(struct storage *self, __u32 node,
 				&generic_storage_ops);
 }
 
-int storage_rw_block(struct storage *self, struct io_request *req);
-
+/**
+ * storage_exit de-initializes the storage instance.
+ *
+ * @self: storage instance.
+ */
 void storage_exit(struct storage *self);
+
+/**
+ * storage_rw_block is invoked by the external modules to request a block io
+ * operation to a storage device. This internally calls @read_block or
+ * @write_block method in @self->ops.
+ *
+ * @self: storage instance.
+ * @req: block io request.
+ *
+ * returns 0 on success, <0 on errors.
+ */
+int storage_rw_block(struct storage *self, struct io_request *req);
 
 #endif	/** __STORAGE_H__ */
 
